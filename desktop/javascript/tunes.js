11bf7a626c0fef7947eef2c6bcdc8a91cadda6b5
unknown <lindsaytbuiwork@gmail.com>
2025-03-02 00:23:11 -0500

    Update March 02

--- desktop/javascript/tunes.js
+++ desktop/javascript/tunes.js
@@ -1,36 +1,36 @@
-let resultsDiv = document.querySelector('#results');
-let juke = document.querySelector('#juke');
+// let resultsDiv = document.querySelector('#results');
+// let juke = document.querySelector('#juke');
 
-function searchMusic() {
-    let query = document.getElementById('searchInput').value;
-    let url = `https://itunes.apple.com/search?term=${encodeURIComponent(query)}&entity=song&limit=10`;  // iTunes API
+// function searchMusic() {
+//     let query = document.getElementById('searchInput').value;
+//     let url = `https://itunes.apple.com/search?term=${encodeURIComponent(query)}&entity=song&limit=10`;  // iTunes API
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            displayResults(data.results);
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             displayResults(data.results);
+//         })
+//         .catch(error => console.error('Error fetching data:', error));
+// }
 
-function displayResults(tracks) {
-    resultsDiv.innerHTML = ''; 
+// function displayResults(tracks) {
+//     resultsDiv.innerHTML = ''; 
 
-    tracks.forEach(track => {
-        let trackDiv = document.createElement('div');
-        trackDiv.className = 'track';
-        trackDiv.innerHTML = `
-            <strong>${track.trackName}</strong><br>
-            <small>${track.artistName}</small><br>
-            <img src="${track.artworkUrl100}" alt="${track.trackName}" width="50">
-        `;
+//     tracks.forEach(track => {
+//         let trackDiv = document.createElement('div');
+//         trackDiv.className = 'track';
+//         trackDiv.innerHTML = `
+//             <strong>${track.trackName}</strong><br>
+//             <small>${track.artistName}</small><br>
+//             <img src="${track.artworkUrl100}" alt="${track.trackName}" width="50">
+//         `;
 
-        trackDiv.onclick = () => player(track.previewUrl);
+//         trackDiv.onclick = () => player(track.previewUrl);
 
-        resultsDiv.appendChild(trackDiv);
-    });
-}
+//         resultsDiv.appendChild(trackDiv);
+//     });
+// }
 
-function player(tune) {
-    juke.innerHTML = `<audio controls autoplay src="${tune}" type='audio/m4a'></audio>`;
-}
+// function player(tune) {
+//     juke.innerHTML = `<audio controls autoplay src="${tune}" type='audio/m4a'></audio>`;
+// }
